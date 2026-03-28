6c8b0e711a97acc532003f8a24cb7a9837566a81
David <159028862+ScreenNamePlus1@users.noreply.github.com>
2026-03-28 17:49:02 -0400

    Initialize settings.gradle.kts with repository settings

---
+++ settings.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+        maven("https://repo.osmdroid.org")
+    }
+}
+
+rootProject.name = "arnav"
+include(":app")
